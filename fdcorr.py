5abf080e65dce3eab9b801828f12791d8c43775d
Filip Dominec <filip.dominec@gmail.com>
2019-06-25 09:48:06 +0200

    multiple working + extra files

--- fdcorr.py
+++ fdcorr.py
@@ -10,7 +10,7 @@ I use scipy.ndimage for image processing.
 
 ## User settings
 
-max_shift=80        ## pixels cropped from the second image determine the maximum shift to be detected (higher number results in slower computation)
+max_shift=60        ## pixels cropped from the second image determine the maximum shift to be detected (higher number results in slower computation)
 decim=2        ## decimation of images prior to correlation (does not affect the results much)
 databarh = 60   ## height of databar at the images' bottom - will be discarded first
 SE_kernel_smoothing = False #  50/decim # optional smoothing of the SE kernel
@@ -29,14 +29,18 @@ from scipy.signal import correlate2d, convolve2d
 from scipy.ndimage.filters import laplace
 np.warnings.filterwarnings('ignore')
 
-def find_shift(im1, im2, smoothing=True, plot_correlation_name=None):
+def find_shift(im1, im2, smoothing=0, plot_correlation_name=None):
     """
     shifts im2 against im1 so that a best correlation is found, returns a tuple of the pixel shift
     """
     ## compute the image correlation, and its smoothed Laplacian
     corr = correlate2d(im1, im2, mode='valid')
-    smoothing_kernel = [[1,2,1],[2,4,2],[1,2,1]] if smoothing else [[1]]
     cr=1  # post-laplace cropping, there were some edge artifacts
+    if smoothing:
+        smoothing_kernel = np.outer(2**-(np.linspace(-2,2,smoothing)**2), 2**-(np.linspace(-2,2,smoothing)**2))
+        smoothing_kernel /= np.sum(smoothing_kernel)
+    else:
+        smoothing_kernel = [[1]]
     laplcorr = convolve2d(-np.abs(laplace(corr)), smoothing_kernel, mode='valid')[cr:-2-cr,cr:-cr] # simple smoothing and removing spurious last 2lines
     vsize, hsize  = laplcorr.shape
 
@@ -58,13 +62,13 @@ def find_shift(im1, im2, smoothing=True, plot_correlation_name=None):
     return [vshift, hshift]
 
 ## Generate a nice rainbow scale
-color_pre_map = np.linspace(0.25, 1, len(sys.argv))
-colors = matplotlib.cm.gist_rainbow_r(color_pre_map*.5 + np.sin(color_pre_map*np.pi/2)**2*.5) * 255
+color_pre_map = np.linspace(0.30, 1, len(sys.argv)-1)
+colors = matplotlib.cm.gist_rainbow_r(color_pre_map*.5 + np.sin(color_pre_map*np.pi/2)**2*.5) 
 
-def paste_overlay(bgimage, fgimage, vs, hs, color, normalize=1):
+def paste_overlay(bgimage, fgimage, vs, hs, color, normalize=np.inf):
     for channel in range(3):
         bgimage[image_padding-vs:image_padding+fgimage.shape[0]-vs, 
-                image_padding-hs:image_padding+fgimage.shape[1]-hs, channel] += fgimage*float(color[channel])/normalize
+                image_padding-hs:image_padding+fgimage.shape[1]-hs, channel] += np.clip(fgimage*float(color[channel])/normalize, 0, 1)
 
 image_padding = max_shift*len(sys.argv[1:]) ## safe very wide black padding, could be optimized!
 
@@ -75,72 +79,91 @@ def safe_imload(imname):
     if len(im.shape) > 2: im = im[:,:,0] # using monochrome images only; strip other channels than the first
     return im, (imname[0] == '+' or ('SC' in imname))
 
-prev_image = None
-cumvshift, cumhshift = 0, 0
-composite_output = None
 channel_outputs = []
 extra_outputs = []
-#np.pad(aa, mode='constant', pad_width=[[1,2],[3,4]])
-for image_name, color in zip(sys.argv[1:], sys.argv[2:], colors):
-    ## Load 2 images, decimate and crop them
-    im2, is_extra2 = safe_imload(imname2)
+for image_name, color in zip(sys.argv[1:], colors):
+    ## Load an image
+    im, is_extra = safe_imload(image_name)
+    im2crop = im[max_shift:-max_shift-databarh:decim, max_shift:-max_shift:decim]*1.0
 
-    if im1 = None:
-        ## Prepare the composite canvas with the first centered image
-        composite_output = [im1.shape[0]+2*image_padding, im1.shape[1]+2*image_padding, 3])*16
-        paste_overlay(composite_output, im1, 0, 0, color, normalize=np.max(im1crop))
-        im1 = im2
-
-    if is_extra: 
-        # export white image (from SEM signal) displaced by cumvshift,cumhshift
-        color = [255,255,255,255]
-        extra_output = [im1.shape[0]+2*image_padding, im1.shape[1]+2*image_padding, 3])*16
-        paste_overlay(extra_output, im1, 0, 0, color, normalize=np.max(im1crop))
-        extra_outputs.append(extra_output)
-        im1 = im2
-    else
-        im1crop = im1[:-databarh:decim, ::decim]*1.0
-        im2crop = im2[max_shift:-max_shift-databarh:decim, max_shift:-max_shift:decim]*1.0
-        #im1 = np.clip(im1, 0, np.max(im1crop)) should prevent color bleaching in the databar?
-        #im2 = np.clip(im2, 0, np.max(im1crop))
-
+    if 'prev_image' in locals():      ## Find shift from previous image
+        im1crop = prev_image[:-databarh:decim, ::decim]*1.0
         ## Find the best correlation of both (cropped) images
-        vshift, hshift = find_shift(im1crop, im2crop, plot_correlation_name=imname2.rsplit('.')[0] + 'alignedto' + imname1.rsplit('.')[0]+'_correlation.png')
+        vshift, hshift = find_shift(im1crop, im2crop, plot_correlation_name=image_name.rsplit('.')[0] + '_correlation.png')
 
-        if not is_extra: 
-            cumvshift += vshift; cumhshift += hshift
-        paste_overlay(output_image, im2, cumvshift, cumhshift, color, normalize=np.max(im2crop))
+        #increment the shifts
+        cumvshift += vshift; cumhshift += hshift 
     else:
-        pass
+        vshift, hshift, cumvshift, cumhshift = 0, 0, 0, 0     ## Initialize position to centre
 
+    if is_extra:  # TODO
+        extra_output = np.zeros([im.shape[0]+2*image_padding, im.shape[1]+2*image_padding, 3])
+        paste_overlay(extra_output, im, cumvshift, cumhshift, [1,1,1,1], normalize=np.max(im2crop))
+        extra_outputs.append(extra_output)
+        cumvshift -= vshift; cumhshift -= hshift  # revert the temporary shift for extra image
+    else:
+        ## Prepare the composite canvas with the first centered image
+        if 'composite_output' not in locals():
+            # TODO extend(composite_output, im.shape[0]+cumvshift, im.shape[1]+cumhshift)
+            composite_output = np.zeros([im.shape[0]+2*image_padding, im.shape[1]+2*image_padding, 3])
+        paste_overlay(composite_output, im, cumvshift, cumhshift, color, normalize=np.max(im2crop))
 
-imageio.imsave('composite_' + '.png', output_image)
+        channel_output = np.zeros([im.shape[0]+2*image_padding, im.shape[1]+2*image_padding, 3])
+        paste_overlay(channel_output, im, cumvshift, cumhshift, color, normalize=np.max(im2crop))
+        channel_outputs.append(channel_output)
+
+        prev_image = im
+
+    # ----
+
+#    if im1 == None:
+#        im1 = im2
+# 
+#    if is_extra: 
+#        # export white image (from SEM signal) displaced by cumvshift,cumhshift
+#        color = [255,255,255,255]
+#        extra_output = [im1.shape[0]+2*image_padding, im1.shape[1]+2*image_padding, 3])*16
+#        paste_overlay(extra_output, im1, 0, 0, color, normalize=np.max(im1crop))
+#        extra_outputs.append(extra_output)
+#        im1 = im2
+#    else
+#        #im1 = np.clip(im1, 0, np.max(im1crop)) should prevent color bleaching in the databar?
+#        #im2 = np.clip(im2, 0, np.max(im1crop))
+# 
+#        ## Find the best correlation of both (cropped) images
+#        vshift, hshift = find_shift(im1crop, im2crop, plot_correlation_name=imname2.rsplit('.')[0] + 'alignedto' + imname1.rsplit('.')[0]+'_correlation.png')
+# 
+#        if not is_extra: 
+#            cumvshift += vshift; cumhshift += hshift
+#        paste_overlay(composite_output, im2, cumvshift, cumhshift, color, normalize=np.max(im2crop))
+#    else:
+#        pass
+# 
+#    prev_image = image
+
+imageio.imsave('composite_' + '.png', composite_output)
+for n,i in enumerate(channel_outputs): imageio.imsave('channel_' + str(n) + '.png', i)
+for n,i in enumerate(extra_outputs): imageio.imsave('extra_' + str(n) + '.png', i)
 
     #np.dstack([im2*256/np.max(im2),im1*256/np.max(im1),im2*256/np.max(im2)])
-
-is_extra1 -> export white
-is_extra2 -> 
-  align
-  export white
-
 
 
 unsharp_kernel = np.outer(2**-(np.linspace(-2,2,unsharp_radius)**2), 2**-(np.linspace(-2,2,unsharp_radius)**2))
 unsharp_kernel /= np.sum(unsharp_kernel)
 unsharp = np.dstack([
-    convolve2d(output_image[:,:,0], unsharp_kernel, mode='same'),
-    convolve2d(output_image[:,:,1], unsharp_kernel, mode='same'),
-    convolve2d(output_image[:,:,2], unsharp_kernel, mode='same')
+    convolve2d(composite_output[:,:,0], unsharp_kernel, mode='same'),
+    convolve2d(composite_output[:,:,1], unsharp_kernel, mode='same'),
+    convolve2d(composite_output[:,:,2], unsharp_kernel, mode='same')
     ])
-output_image = np.clip(output_image*(1+unsharp_weight) - unsharp*unsharp_weight, 0, np.max(output_image))
-#clipped_sample = output_image[output_image.shape[0]//2-100:output_image.shape[0]//2+100, output_image.shape[1]//2-100:output_image.shape[1]//2+100, :]
-#output_image = (output_image - np.min(clipped_sample)) * np.max(output_image) / np.max(clipped_sample)  
+composite_output = np.clip(composite_output*(1+unsharp_weight) - unsharp*unsharp_weight, 0, np.max(composite_output))
+#clipped_sample = composite_output[composite_output.shape[0]//2-100:composite_output.shape[0]//2+100, composite_output.shape[1]//2-100:composite_output.shape[1]//2+100, :]
+#composite_output = (composite_output - np.min(clipped_sample)) * np.max(composite_output) / np.max(clipped_sample)  
 #imageio.imsave('clipped_sample' + '.png', clipped_sample)
-imageio.imsave('composite_sharp' + '.png', output_image)
+imageio.imsave('composite_sharp' + '.png', composite_output)
 
-monochr = np.dstack([np.sum(output_image, axis=2)]*3)
-output_image = np.clip(output_image*(1.5+saturation_enhance) - monochr*saturation_enhance, 0, np.max(output_image))
-imageio.imsave('composite_satu_unsharp' + '.png', output_image)
+monochr = np.dstack([np.sum(composite_output, axis=2)]*3)
+composite_output = np.clip(composite_output*(1.5+saturation_enhance) - monochr*saturation_enhance, 0, np.max(composite_output))
+imageio.imsave('composite_satu_sharp' + '.png', composite_output)
 
 #    ## Composing output image
 #    from PIL import Image
