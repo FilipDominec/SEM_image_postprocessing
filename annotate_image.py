1cf322f2fc17fbbbe745d0ea5a4ed5a93acca67a
Filip Dominec <filip.dominec@gmail.com>
2019-09-22 22:53:12 +0200

    resistence against images w no header

--- annotate_image.py
+++ annotate_image.py
@@ -33,36 +33,61 @@ def im_print(im, text, x, y, color=1):
         if x+cw+cw*n>im.shape[1]: print('Warning, text on image clipped to',text[:n]); break
         else: 
             im[y:y+ch, x+cw*n:x+cw*(n+1)] = match_wb_and_color(im, typecase_dict.get(c, typecase_dict['?'])) * color
-            #im[y:y+ch, x+cw*n:x+cw*(n+1),:] = np.outer(typecase_dict.get(c, typecase_dict['?']), color*np.ones(3))
     return im
 
+
 ## Prepare the logo
-def im_logo(im, logo_img, x, y, color=1):
+def put_image(im, logo_img, x, y, color=1):
     if type(logo_img) is str: logo_img = imageio.imread(logo_img)
     logo_img = match_wb_and_color(im, logo_img[:im.shape[0]-y, :im.shape[1]-x]) ## adjust colors and clip if needed
     im[y:y+logo_img.shape[0], x:x+logo_img.shape[1]] = logo_img * color
     return im
 
 
-#imname = 'test283/channel0_10L415A.png'
-#imname = 'test283/10L415A.TIF'
-#imname = '/home/dominecf/LIMBA/SEM-dle_cisla_vzorku/2019/319B-FH-190613_190625/10M460C.TIF'
-imname = '/home/dominecf/LIMBA/SEM-dle_cisla_vzorku/2019/319B-FH-190613_190625/10NSA.TIF'
+logo_im = imageio.imread('logo_rgb.png') # test
+
+
+## (microscope-dependent settings)
+detectors = {'0': 'SE', '3':'CL'}
+
+
+
+def round125(n):
+    expo = 10**np.floor(np.log10(n))
+    mant = n/expo
+    if mant > 5: return 5*expo
+    if mant > 2: return 2*expo
+    return 1*expo
+
+def putscale(im, x,y,h, xw):
+    try: xw = int(xw)
+    except ValueError: return im
+    white = 255 if len(im.shape) == 2 else np.ones(im.shape[2])*255
+    im[y+2:y+h-2,                     x-1:x+1,     ] = white
+    im[y+2:y+h-2,                     x-1+xw:x+1+xw] = white
+    im[y+int(h/2)-1:y+int(h/2)+1,   x-1:x+1+xw] = white
+    return im
+
+
+
+
+
+imname = sys.argv[1] # '/home/dominecf/LIMBA/SEM-dle_cisla_vzorku/2019/319B-FH-190613_190625/10NSA.TIF'
 
 ## Analyze the TIFF image header specific for Philips/FEI 30XL
-with open(imname, encoding = "ISO-8859-1") as of: 
-    ih = dict(l.strip().split(' = ') for l in of.read().split('\n')[:194] if '=' in l)
-#for par in ('flAccV', 'flSpot', 'lDetName', 'flWD', 'Magnification', 'SizeX', 'SizeY'):
-    #print(par, '=', ih[par])
+try: 
+    with open(imname, encoding = "ISO-8859-1") as of: 
+        ih = dict(l.strip().split(' = ') for l in of.read().split('\n')[:194] if '=' in l)
+except ValueError:
+    ih = {}
 
 
 im = imageio.imread(imname)
 
 
 #logo_im = imageio.imread('logo2.png') # test
-logo_im = imageio.imread('logo_rgb.png') # test
 
-size_x = 117500. / float(ih['Magnification']) 
+size_x = 117500. / float(ih.get('Magnification','nan')) 
 size_y = size_x / 1424*968  / .91 
 if size_x > 1000: 
     size_str = '{:<4f}'.format(size_x/1000)[:4] + 'x' + '{:<4f}'.format(size_y/1000)[:4] + ' mm'
@@ -71,19 +96,6 @@ elif size_x < .1:
 else:                                                                                        
     size_str = '{:<4f}'.format(size_x)[:4]      + 'x' + '{:<4f}'.format(size_y)[:4]            + ' μm'
 
-def putscale(im, x,y,h, xw):
-    white = 255 if len(im.shape) == 2 else np.ones(im.shape[2])*255
-    im[y+2:y+h-2,                     x-1:x+1,     ] = white
-    im[y+2:y+h-2,                     x-1+xw:x+1+xw] = white
-    im[y+int(h/2)-1:y+int(h/2)+1,   x-1:x+1+xw] = white
-    return im
-
-def round125(n):
-    expo = 10**np.floor(np.log10(n))
-    mant = n/expo
-    if mant > 5: return 5*expo
-    if mant > 2: return 2*expo
-    return 1*expo
 scale_bar = round125(size_x/5) # in μm
 if scale_bar > 1000:
     scale_num, scale_unit = scale_bar / 1000, 'mm' 
@@ -91,7 +103,6 @@ elif scale_bar < 1:
     scale_num, scale_unit = scale_bar * 1000, 'nm' 
 else:
     scale_num, scale_unit = scale_bar,        'μm' 
-
 
 
 #print(im.shape)
@@ -104,25 +115,25 @@ print(np.max(im[:int(im.shape[0]*.8),:]))
 xpos = logo_im.shape[1]+10 if im.shape[1]>1200 else 0
 
 
-sample_name, author_name = os.path.basename(os.path.dirname(imname)).replace('-','_').split('_')[:2]
-detectors = {'0': 'SE', '3':'CL'}
+try: sample_name, author_name = os.path.basename(os.path.dirname(imname)).replace('-','_').split('_')[:2]
+except ValueError: sample_name, author_name = '', ''
 
 im = np.pad(im, [(0,ch*4)]+[(0,0)]*(len(im.shape)-1), mode='constant')
-im = im_logo(im, logo_im, x=0, y=int(im.shape[0]-int(ch*4/2)-logo_im.shape[0]/2))
+im = put_image(im, logo_im, x=0, y=int(im.shape[0]-int(ch*4/2)-logo_im.shape[0]/2))
 im = im_print(im, '{:<6} {:<6} {:<6} {:<6} {:<13} {:<8}'.format(
     'AccV', 'Spot', 'WDist', 'Magnif', 'DimXY', 'Scale:'), x=xpos, y=im.shape[0]-ch*4, color=.6)
 im = im_print(im, '{:<.0f} {:}'.format(scale_num, scale_unit), x=xpos+550, y=im.shape[0]-ch*4, color=1)
 im = im_print(im, '{:<6.0f} {:<6.1f} {:<6.2f} {:<6} {:<13}'.format(
-    float(ih['flAccV']), float(ih['flSpot']), float(ih['flWD']), 
-    '{:<.0f}'.format(float(ih['Magnification']))+'x', 
+    float(ih.get('flAccV','nan')), float(ih.get('flSpot','nan')), float(ih.get('flWD','nan')), 
+    '{:<.0f}'.format(float(ih.get('Magnification','nan')))+'x', 
     size_str), x=xpos, y=im.shape[0]-ch*3, color=1)
 im = im_print(im, '{:<13} {:<13} {:<13}'.format('Detector', 'Made', 'Sample name'), x=xpos, y=im.shape[0]-ch*2, color=.6)
 im = im_print(im, '{:<13} {:<13} {:<13}'.format(
-    detectors.get(ih['lDetName'],''), 
+    detectors.get(ih.get('lDetName',''),''), 
     author_name+time.strftime('%Y-%m-%d', time.gmtime(os.path.getmtime(imname))), 
     sample_name), x=xpos, y=im.shape[0]-ch, color=1)
 if im.shape[1]> 1200: im = im_print(im, 'www.fzu.cz/~movpe', x=98, y=im.shape[0]-ch, color=.6)
-im = putscale(im, xpos+465, im.shape[0]-ch*3, ch, int(scale_bar/size_x*im.shape[1]))
+im = putscale(im, xpos+465, im.shape[0]-ch*3, ch, scale_bar/size_x*im.shape[1])
     
 
 imageio.imsave('out.png', im)
