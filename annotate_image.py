bfd9c4e83cec48299ff9002447fb73ef932a4107
Filip Dominec <filip.dominec@gmail.com>
2019-09-23 12:00:37 +0200

    small fixes + image data

--- annotate_image.py
+++ annotate_image.py
@@ -15,21 +15,24 @@ import scipy.ndimage
 
 ## Prepare the font
 typecase_str = ''.join([chr(c) for c in list(range(32,127))+list(range(0x391,0x3a2))+list(range(0x3a3, 0x3aa))+\
-    list(range(0x3b1,0x3c2))+list(range(0x3c3,0x3ca))]) # basic ASCII table + greek 
+    list(range(0x3b1,0x3c2))+list(range(0x3c3,0x3ca))+[0xd7]]) # basic ASCII table + greek 
 try: typecase_img = imageio.imread('typecase.png')
 except FileNotFoundError:
     print('No type set found. To generate one: \n\t1. make a screenshot, \n\t2. convert it to grayscale, '+\
             '\n\t3. crop the text between delimiting blocks and \n\t 4. save it in this folder as typecase.png')
     print(chr(0x2588)+typecase_str.replace('\\',chr(0x29f5))+chr(0x2588)) # (prevents backslash escaping)
-ch, cw = typecase_img.shape[0], int(typecase_img.shape[1]/len(typecase_str)) ## character height and width
+ch, cw = typecase_img.shape[0], round(typecase_img.shape[1]/len(typecase_str)) ## character height and width
+print(ch, cw,typecase_img.shape[1]/len(typecase_str))
 typecase_dict = dict([(c, typecase_img[:,cw*n:cw*n+cw]) for n,c in enumerate(typecase_str)]) ## lookup dict for glyphs
 def match_wb_and_color(im1, im2): 
     ''' Allows to put a grayscale image in a colourful one, and vice versa (in which case it is converted to grays) '''
     if len(im2.shape) > len(im1.shape): im2 = im2[:,:,0] ## reduce channel depth if needed
     if len(im2.shape) < len(im1.shape): im2 = np.dstack([im2]*3)
+    print(im2.shape)
     return im2
 def im_print(im, text, x, y, color=1):
     for n,c in enumerate(text): 
+        print(n,c)
         if x+cw+cw*n>im.shape[1]: print('Warning, text on image clipped to',text[:n]); break
         else: 
             im[y:y+ch, x+cw*n:x+cw*(n+1)] = match_wb_and_color(im, typecase_dict.get(c, typecase_dict['?'])) * color
@@ -47,7 +50,8 @@ def im_logo(im, logo_img, x, y, color=1):
 #imname = 'test283/channel0_10L415A.png'
 #imname = 'test283/10L415A.TIF'
 #imname = '/home/dominecf/LIMBA/SEM-dle_cisla_vzorku/2019/319B-FH-190613_190625/10M460C.TIF'
-imname = '/home/dominecf/LIMBA/SEM-dle_cisla_vzorku/2019/319B-FH-190613_190625/10NSA.TIF'
+#imname = '/home/dominecf/LIMBA/SEM-dle_cisla_vzorku/2019/319B-FH-190613_190625/10NSA.TIF'
+imname = sys.argv[1]
 
 ## Analyze the TIFF image header specific for Philips/FEI 30XL
 with open(imname, encoding = "ISO-8859-1") as of: 
@@ -59,17 +63,16 @@ with open(imname, encoding = "ISO-8859-1") as of:
 im = imageio.imread(imname)
 
 
-#logo_im = imageio.imread('logo2.png') # test
-logo_im = imageio.imread('logo_rgb.png') # test
+logo_im = imageio.imread('logo.png') # test
 
 size_x = 117500. / float(ih['Magnification']) 
 size_y = size_x / 1424*968  / .91 
 if size_x > 1000: 
-    size_str = '{:<4f}'.format(size_x/1000)[:4] + 'x' + '{:<4f}'.format(size_y/1000)[:4] + ' mm'
-elif size_x < .1:                                                                      
-    size_str = '{:<4f}'.format(size_x*1000)[:4] + 'x' + '{:<4f}'.format(size_y*1000)[:4]  + ' nm'
-else:                                                                                        
-    size_str = '{:<4f}'.format(size_x)[:4]      + 'x' + '{:<4f}'.format(size_y)[:4]            + ' μm'
+    size_str = '{:<4f}'.format(size_x/1000)[:4] + '×' + '{:<4f}'.format(size_y/1000)[:4] + ' mm'
+elif size_x < .1:                                                                     
+    size_str = '{:<4f}'.format(size_x*1000)[:4] + '×' + '{:<4f}'.format(size_y*1000)[:4]  + ' nm'
+else:                                                                                       
+    size_str = '{:<4f}'.format(size_x)[:4]      + '×' + '{:<4f}'.format(size_y)[:4]            + ' μm'
 
 def putscale(im, x,y,h, xw):
     white = 255 if len(im.shape) == 2 else np.ones(im.shape[2])*255
@@ -101,28 +104,30 @@ print(np.max(im[:int(im.shape[0]*.8),:]))
 im = np.clip(im * 256. / np.max(im[:int(im.shape[0]*.8),:]),0, 255)
 
 print(np.max(im[:int(im.shape[0]*.8),:]))
-xpos = logo_im.shape[1]+10 if im.shape[1]>1200 else 0
+xpos = logo_im.shape[1]+10 if im.shape[1]>logo_im.shape[1]+cw*55 else 0
 
-
-sample_name, author_name = os.path.basename(os.path.dirname(imname)).replace('-','_').split('_')[:2]
+try: sample_name, author_name = os.path.basename(os.path.dirname(imname)).replace('-','_').split('_')[:2]
+except ValueError: sample_name, author_name = '', ''
 detectors = {'0': 'SE', '3':'CL'}
 
 im = np.pad(im, [(0,ch*4)]+[(0,0)]*(len(im.shape)-1), mode='constant')
 im = im_logo(im, logo_im, x=0, y=int(im.shape[0]-int(ch*4/2)-logo_im.shape[0]/2))
 im = im_print(im, '{:<6} {:<6} {:<6} {:<6} {:<13} {:<8}'.format(
     'AccV', 'Spot', 'WDist', 'Magnif', 'DimXY', 'Scale:'), x=xpos, y=im.shape[0]-ch*4, color=.6)
-im = im_print(im, '{:<.0f} {:}'.format(scale_num, scale_unit), x=xpos+550, y=im.shape[0]-ch*4, color=1)
+im = im_print(im, '{:<.0f} {:}'.format(
+    scale_num, scale_unit), x=xpos+cw*49, y=im.shape[0]-ch*4, color=1)
 im = im_print(im, '{:<6.0f} {:<6.1f} {:<6.2f} {:<6} {:<13}'.format(
     float(ih['flAccV']), float(ih['flSpot']), float(ih['flWD']), 
-    '{:<.0f}'.format(float(ih['Magnification']))+'x', 
+    '{:<.0f}'.format(float(ih['Magnification']))+'×', 
     size_str), x=xpos, y=im.shape[0]-ch*3, color=1)
-im = im_print(im, '{:<13} {:<13} {:<13}'.format('Detector', 'Made', 'Sample name'), x=xpos, y=im.shape[0]-ch*2, color=.6)
+im = im_print(im, '{:<13} {:<13} {:<13}'.format(
+    'Detector', 'Made', 'Sample name'), x=xpos, y=im.shape[0]-ch*2, color=.6)
 im = im_print(im, '{:<13} {:<13} {:<13}'.format(
     detectors.get(ih['lDetName'],''), 
     author_name+time.strftime('%Y-%m-%d', time.gmtime(os.path.getmtime(imname))), 
     sample_name), x=xpos, y=im.shape[0]-ch, color=1)
-if im.shape[1]> 1200: im = im_print(im, 'www.fzu.cz/~movpe', x=98, y=im.shape[0]-ch, color=.6)
-im = putscale(im, xpos+465, im.shape[0]-ch*3, ch, int(scale_bar/size_x*im.shape[1]))
+if xpos > 0: im = im_print(im, 'www.fzu.cz/~movpe', x=8, y=im.shape[0]-ch, color=.6)
+im = putscale(im, xpos+cw*42, im.shape[0]-ch*3, ch, int(scale_bar/size_x*im.shape[1]))
     
 
 imageio.imsave('out.png', im)
