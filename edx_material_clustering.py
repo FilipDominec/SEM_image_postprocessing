4989d68851d9615b6b4f9ea60432b4815be32067
Filip Dominec <filip.dominec@gmail.com>
2020-08-17 10:00:48 +0200

    cleanup

--- edx_material_clustering.py
+++ edx_material_clustering.py
@@ -10,6 +10,7 @@
 print(__doc__)
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm
 from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances_argmin
 from sklearn.datasets import load_sample_image
@@ -21,13 +22,15 @@ import imageio, sys
 from scipy.ndimage.filters import gaussian_filter
 
 
-# Load the input images
-#input_layers = load_sample_image("input_layers.jpg")
-#imnames = ('~/p/200608_LED/M2/edxmap_new/map2_AlK.png', '~/p/200608_LED/M2/edxmap_new/map2_CuK.png', 
-       #'~/p/200608_LED/M2/edxmap_new/map2_OK.png', '~/p/200608_LED/M2/edxmap_new/map2_GaK.png', '~/p/200608_LED/M2/edxmap_new/map2_OK.png')
+# User input and settings
+n_colors = int(sys.argv[1])
+imnames  = sys.argv[2:]
 
+
+# Load the input images
 def load_Siemens_BMP(fname):
-    """ Experimental loading of BMPs from Siemens microscopes (atypical - that cannot be loaded by imageio)
+    """ 
+    Experimental loading of BMPs from Siemens microscopes (atypical - that cannot be loaded by imageio)
     See https://ide.kaitai.io/ for more information on BMP header. 
     """
     with open(fname, mode='rb') as file: # first analyze the header
@@ -38,23 +41,15 @@ def load_Siemens_BMP(fname):
     assert compr == 0, 'no decompression algorithm implemented'
     return np.fromfile(fname, dtype=np.uint8)[ofs:ofs+w*h].reshape(h,w)[::-1,:] # BMP is "upside down" - flip vertically
 
-n_colors = int(sys.argv[1])
-imnames  = sys.argv[2:]
-#for imname in imnames: print('LOADING', imname, imageio.imread(imname).shape)
-
-print(imnames)
 input_layers = []
 for imname in imnames:
-    try: 
-        input_layer = imageio.imread(imname)
-    except:
-        input_layer = load_Siemens_BMP(imname)
-    print(input_layer,input_layer.shape)
-    #input_layer = input_layer * float(len(np.unique(input_layer))-1) / np.max(input_layer) ## normalize - the more unique levels found, the more EDX signal there was
-    input_layer = input_layer * float(len(np.unique(input_layer))-1)**.5 / np.max(input_layer) ## normalize - the more unique levels found, the more EDX signal there was
-    #input_layer = input_layer  / np.max(input_layer) ## normalize - the more unique levels found, the more EDX signal there was
-    #input_layers.append(gaussian_filter(255-np.sum(imageio.imread(imname),axis=0), sigma=2)) ## slightly smear
-    input_layers.append(gaussian_filter(input_layer, sigma=1.)) ## slightly smear
+    try: input_layer = imageio.imread(imname)
+    except: input_layer = load_Siemens_BMP(imname)
+
+    ## partial de-normalization: the more unique levels found in each image, the more EDX signal there was 
+    input_layer = input_layer * float(len(np.unique(input_layer))-1)**.5 / np.max(input_layer) 
+    ## slightly smear
+    input_layers.append(gaussian_filter(input_layer, sigma=1.5)) 
 input_layers = np.dstack(input_layers)
 
 
@@ -63,41 +58,34 @@ input_layers = np.dstack(input_layers)
 #input_layers = np.array(input_layers, dtype=np.float64) / 255
 
 # Load Image and transform to a 2D numpy array.
-w, h, d = original_shape = tuple(input_layers.shape)
+w, h, d = input_layers.shape
 image_array = np.reshape(input_layers, (w * h, d))
 
-print("Fitting model on a small sub-sample of the data")
-t0 = time()
+## Guess the optimum number of clusters (todo: should another algorithm be chosen?)
+scores = []
+for n_colors in range(3,len(imnames)+1): # subjectively proposed range of colour count to test
+    t0 = time()
+    image_array_sample = shuffle(image_array, random_state=0)[:100]
+    kmeans = KMeans(n_clusters=n_colors, random_state=0).fit(image_array_sample)
+    #print('n_colors, kmeans.inertia, c*i',n_colors, kmeans.inertia_, n_colors, kmeans.inertia_*n_colors**1.2)
+    scores.append((kmeans.inertia_*n_colors**(1+1./len(imnames)), n_colors))
+    #print("done in %0.3fs." % (time() - t0))
+
+    # Get labels for all points
+    t0 = time()
+    labels = kmeans.predict(image_array)
+    #print("done in %0.3fs." % (time() - t0))
+scores.sort()
+_, n_colors = scores[0]
+n_colors = 12
+print(scores,n_colors)
+
+
+## Actual clustering on larger sample
 image_array_sample = shuffle(image_array, random_state=0)[:1000]
 kmeans = KMeans(n_clusters=n_colors, random_state=0).fit(image_array_sample)
-print("done in %0.3fs." % (time() - t0))
-
-# Get labels for all points
-print("Predicting color indices on the full image (k-means)")
 t0 = time()
 labels = kmeans.predict(image_array)
-print("done in %0.3fs." % (time() - t0))
-
-
-#codebook_random = shuffle(image_array, random_state=0)[:n_colors]
-#print("Predicting color indices on the full image (random)")
-#t0 = time()
-#labels_random = pairwise_distances_argmin(codebook_random,
-                                          #image_array,
-                                          #axis=0)
-#print("done in %0.3fs." % (time() - t0))
-
-
-def recreate_image(codebook, labels, w, h):
-    """Recreate the (compressed) image from the code book & labels"""
-    d = codebook.shape[1]
-    image = np.zeros((w, h, d))
-    label_idx = 0
-    for i in range(w):
-        for j in range(h):
-            image[i][j] = codebook[labels[label_idx]]
-            label_idx += 1
-    return image
 
 # Display all results, alongside original image
 #plt.figure(1)
@@ -115,10 +103,9 @@ plt.axis('off')
 ax1.set_title('Quantized image ({:d} colors, K-Means)'.format(n_colors))
 #ax1.imshow(recreate_image(kmeans.cluster_centers_, labels, w, h))
 
-print(kmeans.cluster_centers_, )
+## Reorder the cluster and label arrays so that 
 idx = np.arange(len(kmeans.cluster_centers_), dtype=int)
-print(kmeans.cluster_centers_[idx])
-for n in range(1000):
+for n in range(10000):
     newidx = shuffle(idx, random_state=n)
     newmetric = np.sum((kmeans.cluster_centers_[newidx][:-1]-kmeans.cluster_centers_[newidx][1:])**2)
     if 'bestmetric' not in locals() or newmetric < bestmetric:
@@ -126,11 +113,23 @@ for n in range(1000):
         bestmetric = newmetric
 kmeans.cluster_centers_ = kmeans.cluster_centers_[bestidx]
 label_dict = dict(zip(bestidx, idx))
-print('label_dict', label_dict,zip(idx, bestidx))
 labels = [label_dict[x] for x in labels]
 
-import matplotlib.cm
+
+## Generate color coding
 my_palette = matplotlib.cm.gist_rainbow(np.linspace(0, 1, n_colors+1)[:-1])
+
+def recreate_image(codebook, labels, w, h): ## todo: obsolete?
+    """Recreate the (compressed) image from the code book & labels"""
+    d = codebook.shape[1]
+    image = np.zeros((w, h, d))
+    label_idx = 0
+    for i in range(w):
+        for j in range(h):
+            image[i][j] = codebook[labels[label_idx]]
+            label_idx += 1
+    return image
+
 ax1.imshow(recreate_image(my_palette, labels, w, h))
 
 for color, clustcent in zip(my_palette, kmeans.cluster_centers_):
